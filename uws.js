cfdf55769a2167e7bc706ad5daf0cce194e9f6ff
Luis Vilca Flores <ferluisxd@gmail.com>
2020-07-01 11:39:33 -0500

    feat(user)

--- uws.js
+++ uws.js
@@ -19,26 +19,34 @@ app.ws('/*', {
   },
   message: (ws, message, isBinary) => {
     /* Ok is false if backpressure was built up, wait for drain */
-    
+
     // console.log(ws,message,isBinary)
     var arr = new Uint8Array(message);
     var str = String.fromCharCode.apply(String, arr);
     let obj = JSON.parse(str)
-    socketList[ws.id].lastMessage =obj.message 
-    let string = JSON.stringify({message: obj.message, user: ws.id })
-    // let ok = ws.send(, isBinary);
-    sendToEveryone(string)
+    switch (obj.method) {
+      case 'chat':
+        chatNewMessage(ws, obj)
+        break;
+      case 'new':
+        addUser(ws, obj)
+        break;
+      case 'chatt':
+        break;
+      case 'chattt':
+        break;
+    }
   },
   drain: (ws) => {
     console.log('WebSocket backpressure: ' + ws.getBufferedAmount());
   },
   close: (ws, code, message) => {
-    delete socketList[ws.id]  
+    delete socketList[ws.id]
     console.log('WebSocket closed');
   }
 })
 
-app .any('/*', (res, req) => {
+app.any('/*', (res, req) => {
   res.end('Nothing to see here!');
 })
 
@@ -50,19 +58,32 @@ app.listen(port, (token) => {
   }
 });
 
-function sendToEveryone(msg){
-  for(let i in socketList){
+function sendToEveryone(msg) {
+  for (let i in socketList) {
     let socket = socketList[i]
-    if(socket.id)
-      socket.send(msg) 
+    if (socket.id)
+      socket.send(msg)
   }
 }
-setInterval(()=>{
+setInterval(() => {
   let messages = []
-  for(let i in socketList){
+  for (let i in socketList) {
     let socket = socketList[i]
-    if(socket.lastMessage)
-      messages.push(socket.lastMessage) 
+    if (socket.lastMessage)
+      messages.push(socket.lastMessage)
   }
-  console.log(messages)
-},2000)
+  console.log(messages, socketList)
+}, 2000)
+
+function chatNewMessage(ws, obj) {
+  socketList[ws.id].lastMessage = obj.message
+  let string = JSON.stringify({ message: obj.message, user: ws.id })
+  // let ok = ws.send(, isBinary);
+  sendToEveryone(string)
+}
+function addUser(ws, obj) {
+  console.log('new user added?')
+  delete socketList[ws.id]
+  ws.id = obj.user
+  socketList[ws.id] = ws
+}
